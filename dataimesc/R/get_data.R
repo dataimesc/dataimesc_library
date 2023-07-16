90d3852c061345472e7fb839aa99cb514d8639cb
DATAIMESC <137085586+dataimesc@users.noreply.github.com>
2023-07-16 16:08:12 -0300

    Add files via upload

---
+++ dataimesc/R/get_data.R
@@ -0,0 +1,12 @@
+get_data<-function(number, year_i,year_f){
+  require(tidyverse)
+  require(jsonlite)
+  require(httr)
+  x<-httr::GET(str_interp('https://dataimesc.imesc.ma.gov.br/getData?id=${number}8&scope=4&from=${year_i}&to=${year_f}'), accept_json())
+  jsonInfoImg <- content(x, type="application/json")
+  table<-as.matrix(unlist(jsonInfoImg['values']))
+  return(jsonInfoImg['values'])
+}
+
+
+
