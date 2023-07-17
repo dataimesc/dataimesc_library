2df570f12a063e53de0143007e83b506853d5998
DATAIMESC <137085586+dataimesc@users.noreply.github.com>
2023-07-17 11:36:54 -0300

    Update get_data.R

--- dataimesc/R/get_data.R
+++ dataimesc/R/get_data.R
@@ -6,8 +6,8 @@ get_data<-function(number, year_i,year_f){
   require(httr)
   x<-httr::GET(str_interp('https://dataimesc.imesc.ma.gov.br/getData?id=${number}&scope=4&from=${year_i}&to=${year_f}'), accept_json())
   jsonInfoImg <- content(x, type="application/json")
-  table<-as.matrix(unlist(jsonInfoImg['values']))
-  return(jsonInfoImg['values'])
+  table<-as.data.frame(unlist(jsonInfoImg['values']))
+  return(table)
 }
 
 
