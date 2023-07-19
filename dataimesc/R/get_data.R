f43ac82649e528813d13e79ae8f2594f5cce8cfd
DATAIMESC <137085586+dataimesc@users.noreply.github.com>
2023-07-18 21:12:25 -0300

    Update get_data.R

--- dataimesc/R/get_data.R
+++ dataimesc/R/get_data.R
@@ -1,10 +1,10 @@
-get_data<-function(serie, year_init,year_last){
+get_data<-function(serie, begin, end){
   if(!require("tidyverse")) install.packages("tidyverse")
   if(!require("jsonlite")) install.packages("jsonlite")
   if(!require("httr")) install.packages("httr")
   require(jsonlite)
   require(httr)
-  json<-httr::GET(str_interp('https://dataimesc.imesc.ma.gov.br/getData?id=${number}&scope=4&from=${year_i}&to=${year_f}'), accept_json())
+  json<-httr::GET(str_interp('https://dataimesc.imesc.ma.gov.br/getData?id=${serie}&scope=4&from=${begin}&to=${end}'), accept_json())
   jsonInfoImg <- content(json, type="application/json")
   table_json<-as.data.frame(unlist(jsonInfoImg['values']))
   return(table_json)
