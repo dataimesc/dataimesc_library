5fb2e595ac7b97a2e370729f401bae7fc721c546
DATAIMESC <137085586+dataimesc@users.noreply.github.com>
2023-09-11 10:35:32 -0300

    Update info_imesc.R

--- dataimesc/R/info_imesc.R
+++ dataimesc/R/info_imesc.R
@@ -1,7 +1,8 @@
 info_imesc<-function(serie){
   if(!require("httr")) install.packages("httr")
+  if(!require("tidyverse")) install.packages("tidyverse")
   require(httr)
   json<-httr::GET(str_interp('https://dataimesc.imesc.ma.gov.br/getDetailsIndicators?id=${serie}'), accept_json())
   jsonInfoImg <- content(json, type="application/json")
-  return(jsonInfoImg)
+  return(cat(str_interp(' O número da série é ${serie}\n, o nome da série é: ${jsonInfoImg$name}\n, A unidade da série é ${jsonInfoImg$unity_name}\n  a Frequência da série é ${jsonInfoImg$frequency_name}')))
 }
