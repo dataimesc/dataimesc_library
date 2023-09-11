8ab9bafef7526a81b88859f86bc32380aa23529b
DATAIMESC <137085586+dataimesc@users.noreply.github.com>
2023-09-11 10:26:56 -0300

    Update info_imesc.R

--- dataimesc/R/info_imesc.R
+++ dataimesc/R/info_imesc.R
@@ -1,1 +1,7 @@
-
+info_imesc<-function(serie){
+  if(!require("httr")) install.packages("httr")
+  require(httr)
+  json<-httr::GET(str_interp('https://dataimesc.imesc.ma.gov.br/getDetailsIndicators?id=${serie}'), accept_json())
+  jsonInfoImg <- content(json, type="application/json")
+  return(jsonInfoImg)
+}
